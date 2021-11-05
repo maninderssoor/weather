b5ce6a19dfab2a3d7d4ada7e2a1f3b19e598a4f6
Maninder Soor <msoor@triller.co>
2021-11-05 21:47:54 +0000

    Styling and UI fixes for navigation bar and status bar

---
+++ sample/View Controllers/NavigationController.swift
@@ -0,0 +1,9 @@
+import Foundation
+import UIKit
+
+class NavigationController: UINavigationController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .darkContent
+    }
+}
